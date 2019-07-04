35d95b450c6f6ba63480b997a01b2f8646c59ef4
David Gallego Gomez <davizs@hotmail.com>
2019-07-05 00:33:46 +0200

    feat: create main layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import Login from './components/auth/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Layout, Menu, Icon, Avatar } from "antd";
+const { Header, Content, Sider } = Layout;
+
+class App extends Component {
+  state = { collapsed: false };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <h1 style={{ color: 'white' }}>Travel Xperience</h1>
+          <Avatar className="avatar" shape="square" size="large" icon="user" />
+        </Header>
+        <Layout>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}
+          >
+            <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+              <Menu.Item key="1">
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="desktop" />
+                <span>Option 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="user" />
+                <span>User</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="team" />
+                <span>Team</span>
+              </Menu.Item>
+              <Menu.Item key="5">
+                <Icon type="file" />
+                <span>File</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: "24px" }}>
+            <Content
+              style={{
+                background: "#fff",
+                padding: 24,
+                margin: 0,
+                // minHeight: 280,
+                height: "calc(79vh)" // TODO: revisar para que quede al 100% sin su padding (¿funciona 'calc'?)
+              }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 export default App;
